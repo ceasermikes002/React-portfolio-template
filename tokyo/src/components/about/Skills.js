fcb6d4bdfd7b12ae2ec4e577b883224295163c76
chima <chimaemekaiheonu@gmail.com>
2023-09-29 13:45:05 +0100

    Description of the changes you made

---
+++ tokyo/src/components/about/Skills.js
@@ -0,0 +1,68 @@
+const programming = [
+  { id: 1, name: "Next.JS", value: 95 },
+  { id: 2, name: "HTML & CSS", value: 80 },
+  { id: 3, name: "Flutter", value: 90 },
+  { id: 4, name: "C,C++,C#", value: 78 },
+];
+const language = [
+  { id: 1, name: "English", value: 90 },
+  { id: 2, name: "French", value: 55 },
+  { id: 3, name: "Yoruba", value: 47 },
+];
+const Skills = () => {
+  return (
+    <div className="tokyo_tm_progressbox w-full h-auto clear-both float-left bg-white pt-[93px] pr-[0px] pb-[100px] pl-[0px]">
+      <div className="container">
+        <div className="in w-full h-auto clear-both float-left flex">
+          <div className="left w-1/2 pr-[50px]">
+            <div className="tokyo_section_title w-full h-auto clear-both float-left mb-[40px]">
+              <h3 className="text-[20px] font-bold">Programming Skills</h3>
+            </div>
+            <div className="tokyo_progress">
+              {programming.map((skill) => (
+                <div className="progress_inner" key={skill.id}>
+                  <span>
+                    <span className="label">{skill.name}</span>
+                    <span className="number">{skill.value}%</span>
+                  </span>
+                  <div className="background">
+                    <div className="bar">
+                      <div
+                        className="bar_in"
+                        style={{ width: `${skill.value}%` }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="right w-1/2 pl-[50px]">
+            <div className="tokyo_section_title w-full h-auto clear-both float-left mb-[40px]">
+              <h3 className="text-[20px] font-bold">Language Skills</h3>
+            </div>
+            <div className="tokyo_progress">
+              {language.map((skill) => (
+                <div className="progress_inner" key={skill.id}>
+                  <span>
+                    <span className="label">{skill.name}</span>
+                    <span className="number">{skill.value}%</span>
+                  </span>
+                  <div className="background">
+                    <div className="bar">
+                      <div
+                        className="bar_in"
+                        style={{ width: `${skill.value}%` }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Skills;
